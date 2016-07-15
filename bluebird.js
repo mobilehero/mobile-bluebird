9e806d57e29e717870c6823effaa7383ec0cf3b4
Brenton House <brenton.house@gmail.com>
2016-07-15 14:14:23 -0500

    :checkered_flag: v3.1.3 :heavy_minus_sign: Repackaging from: https://www.npmjs.com/package/bluebird

--- bluebird.js
+++ bluebird.js
@@ -23,7 +23,7 @@
  * 
  */
 /**
- * bluebird build version 3.1.2
+ * bluebird build version 3.1.3
  * Features enabled: core, race, call_get, generators, map, nodeify, promisify, props, reduce, settle, some, using, timers, filter, any, each
 */
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.Promise=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof _dereq_=="function"&&_dereq_;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof _dereq_=="function"&&_dereq_;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
@@ -1890,7 +1890,6 @@ var util = _dereq_("./util");
 var errorObj = util.errorObj;
 var tryCatch = util.tryCatch;
 var yieldHandlers = [];
-var defineProperty = _dereq_("./es5").defineProperty;
 
 function promiseFromYieldHandler(value, yieldHandlers, traceParent) {
     for (var i = 0; i < yieldHandlers.length; ++i) {
@@ -2053,8 +2052,7 @@ Promise.coroutine = function (generatorFunction, options) {
     var yieldHandler = Object(options).yieldHandler;
     var PromiseSpawn$ = PromiseSpawn;
     var stack = new Error().stack;
-
-    function wrap () {
+    return function () {
         var generator = generatorFunction.apply(this, arguments);
         var spawn = new PromiseSpawn$(undefined, undefined, yieldHandler,
                                       stack);
@@ -2062,16 +2060,7 @@ Promise.coroutine = function (generatorFunction, options) {
         spawn._generator = generator;
         spawn._promiseFulfilled(undefined);
         return ret;
-    }
-
-    defineProperty(wrap, "length", {
-      value: generatorFunction.length,
-      writable: false,
-      enumerable: false,
-      configurable: true
-    });
-
-    return wrap;
+    };
 };
 
 Promise.coroutine.addYieldHandler = function(fn) {
@@ -2093,7 +2082,7 @@ Promise.spawn = function (generatorFunction) {
 };
 };
 
-},{"./errors":12,"./es5":13,"./util":36}],17:[function(_dereq_,module,exports){
+},{"./errors":12,"./util":36}],17:[function(_dereq_,module,exports){
 "use strict";
 module.exports =
 function(Promise, PromiseArray, tryConvertToPromise, INTERNAL) {
